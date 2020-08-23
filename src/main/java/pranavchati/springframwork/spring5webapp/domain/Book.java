e51f31da38d052b43b9e6d71db39258162911793
pranavchati <pchati2003@gmail.com>
2020-08-22 22:17:33 -0400

    First Commit

---
+++ src/main/java/pranavchati/springframwork/spring5webapp/domain/Book.java
@@ -0,0 +1,67 @@
+package pranavchati.springframwork.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Pranav Chati on 8/20/20
+ */
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    //Constructor
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Set<Author> author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = author;
+    }
+
+    
+    //Setter and Getters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> author) {
+        this.authors = author;
+    }
+}

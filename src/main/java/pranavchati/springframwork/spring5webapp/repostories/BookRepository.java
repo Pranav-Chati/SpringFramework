09f18c9d2a8d743b6f1defcbda035be9f98c4ec6
pranavchati <pchati2003@gmail.com>
2020-08-22 22:50:25 -0400

    8/22/20 - Added comment with name and date

--- src/main/java/pranavchati/springframwork/spring5webapp/repostories/BookRepository.java
+++ src/main/java/pranavchati/springframwork/spring5webapp/repostories/BookRepository.java
@@ -2,7 +2,9 @@ package pranavchati.springframwork.spring5webapp.repostories;
 
 import org.springframework.data.repository.CrudRepository;
 import pranavchati.springframwork.spring5webapp.domain.Book;
-
+/**
+ * Create by Pranav Chati ON 8/22/20
+ */
 public interface BookRepository extends CrudRepository<Book, Long> {
     
 }
